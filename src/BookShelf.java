12fd5c7c5a9445c21d00f9151a21389184c2772f
d1mk0v <79874763588@ya.ru>
2023-08-23 10:36:03 +0500

    done

--- src/BookShelf.java
+++ src/BookShelf.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class BookShelf {
     public static void main(String[] args) {
-
         List<String> books = Arrays.asList(
                 "Вий", "Азбука", "Мастер и Маргарита",
                 "Война и мир", "Преступление и наказание", "Белая гвардия"
